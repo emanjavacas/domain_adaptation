5d501838b2d32ff1bda20b827dbe37cf64052029
emanjavacas <enrique.manjavacas@gmail.com>
2016-01-01 16:03:45 +0100

    backup

--- seqlearn_reader.py
+++ seqlearn_reader.py
@@ -1,21 +1,29 @@
 
-from sklearn.feature_extraction import FeatureHasher
+from collections import Counter
+
+from sklearn.feature_extraction import FeatureHasher, DictVectorizer
 
 
-def features(words, tags, i, window=2):
-    length = len(words)
-    target = words[i]
+def features(tokens, tags, i, window=2):
+    length = len(tokens)
+    target = tokens[i]
     yield 'bias'
     if target[0].isupper():
         yield 'CAP'
-    yield 'prefix:{}' + target[0]
+    yield 'prefix:{%s}' % target[0]
     first, last = max(0, i - window), min(length - 1, i + window + 1)
     for idx in range(first, last):
-        word = words[idx]
-        yield 'word[%d]:{}%s' % (idx - i, word.lower())
-        yield 'suffix[%d]:{}%s' % (idx - i, word[-3:])
-        if idx < i:
-            yield 'tag[%d]:{}%s' % (idx - i, tags[idx])
+        word = tokens[idx]
+        yield 'word[%d]:{%s}' % (idx - i, word.lower())
+        yield 'suffix[%d]:{%s}' % (idx - i, word[-3:])
+        if idx < i and tags:
+            yield 'tag[%d]:{%s}' % (idx - i, tags[idx])
+
+
+def clf_predict(tokens, dv, clf, window=2):
+    tags = []
+    for i in range(len(tokens)):
+        feats = features(tokens, tags, i, window)
 
 
 def sent_sequences(sents, feature_fn, labels, lengths):
@@ -28,9 +36,38 @@ def sent_sequences(sents, feature_fn, labels, lengths):
             yield features(words, tags, i)
 
 
-def load_data(sents, feature_fn):
+def load_data(sents, features=features):
     y, lengths = [], []
-    X_raw = sent_sequences(sents, feature_fn, y, lengths)
+    X_raw = sent_sequences(sents, features, y, lengths)
     fh = FeatureHasher(input_type='string')
     X = fh.transform(X_raw)
     return X, y, lengths
+
+
+def load_data_dict(sents, dv, features=features):
+    y, lengths = [], []
+    X_raw = sent_sequences(sents, features, y, lengths)
+    X = dv.fit_transform(Counter(s) for s in X_raw)
+    return X, y, lengths
+
+
+# from penn_data import pos_from_range
+# from seqlearn.perceptron import StructuredPerceptron
+
+sents = pos_from_range(1400, 1500, 50)
+dv = DictVectorizer()
+X, y, lengths = load_data_dict(sents, dv)
+split_lengths = int(len(lengths) * 0.9)
+split = sum(lengths[:split_lengths])
+X_train, y_train, lengths_train = X[:split,], y[:split], lengths[:split_lengths]
+X_test, y_test, lengths_test = X[split:,], y[split:], lengths[split_lengths:]
+clf = StructuredPerceptron(verbose=True, max_iter=10)
+clf.fit(X_train, y_train, lengths_train)
+
+feats = 
+
+# dv = DictVectorizer()
+# D = [{'dog': 1, 'cat': 2, 'elephant': 4}, {'dog': 2, 'run': 5}]
+# C = [{'bear': 1, 'butterfly': 2, 'deer': 4}, {'deer': 2, 'apple': 5}]
+# DD = dv.fit_transform(D)
+# CC = dv.transform(C)
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cPickle as p
 from datetime import datetime
+from collections import defaultdict
 
 
 def take(g, n):
@@ -17,7 +18,7 @@ def shuffle(seq, seed=448):
     return sorted(seq, key=lambda k: random.random())
 
 
-def plot_conf_matrix(cm, labels, title='Confusion Matrix', cmap=plt.cm.Blues):
+def plot_conf_matrix(cm, labels, title='Confusion Matrix', cmap=plt.cm.OrRd):
     plt.imshow(cm, interpolation='nearest', cmap=cmap, aspect='auto')
     plt.title(title)
     plt.colorbar()
@@ -27,6 +28,29 @@ def plot_conf_matrix(cm, labels, title='Confusion Matrix', cmap=plt.cm.Blues):
     plt.tight_layout()
     plt.xlabel('Predicted label', fontsize=14)
     plt.ylabel('True label', fontsize=14)
+
+
+def normalize_cm(cm):
+    total_rows = np.asarray(sum(np.transpose(cm)), dtype=np.float)
+    with np.errstate(divide='ignore', invalid='ignore'):
+        cm_norm = cm / total_rows
+        cm_norm[cm_norm == np.inf] = 0
+        cm_norm = np.nan_to_num(cm_norm)
+        return cm_norm
+
+
+def per_tag_scores(y_true, y_pred):
+    scores = defaultdict(lambda: defaultdict(int))
+    assert len(y_true) == len(y_pred)
+    for i in range(len(y_true)):
+        true_tag = y_true[i]
+        guessed_tag = y_pred[i]
+        if true_tag == guessed_tag:
+            scores[true_tag]['truePositives'] += 1
+        else:
+            scores[true_tag]['falseNegatives'] += 1
+            scores[guessed_tag]['falsePositives'] += 1
+    return dict(scores)
 
 
 def pickle_this(fname, obj):
